b29fc08bab737161952785ac0bd88b52fbd85f7d
masashiyokoi <masashi.yokoi@yokoiinfocom.co.jp>
2019-01-05 20:45:44 +0900

    Add image column to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,6 @@ class User < ApplicationRecord
   acts_as_followable
   acts_as_follower
 
+  mount_uploader :image, ImageUploader
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_01_112147) do
+ActiveRecord::Schema.define(version: 2019_01_05_113340) do
 
   create_table "activities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "trackable_type"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2019_01_01_112147) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "profile"
     t.string "name", null: false
+    t.string "image"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
