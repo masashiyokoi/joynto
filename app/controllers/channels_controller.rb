6d4365b72c24cb508e7fa3e63a30d4c652a98ba9
masashiyokoi <masashi.yokoi@yokoiinfocom.co.jp>
2019-02-11 21:25:19 +0900

    Fix ChannelsController

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,10 +1,8 @@
 class ChannelsController < ApplicationController
-  before_action :set_timeline, only: [:show, :edit, :update, :destroy, :like, :unvote]
-  before_action :check_current_user, only: [:edit, :update, :destroy]
+  before_action :check_current_user
 
   def index
-    @channels = Channel.where(type: 'private')
-      .joins(:channel_users)
+    @channels = Channel.joins(:channel_users)
       .where(channel_users: { user_id: current_user.id })
   end
 end
