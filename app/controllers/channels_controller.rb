42a994b26b7beff44d5f12355c66a08452b9c962
masashiyokoi <masashi.yokoi@yokoiinfocom.co.jp>
2019-02-11 22:10:10 +0900

    Add ChannlesController show action and more..

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,8 +1,19 @@
 class ChannelsController < ApplicationController
-  before_action :check_current_user
+  before_action :set_channel, only: [:show, :edit, :update, :destroy]
 
   def index
     @channels = Channel.joins(:channel_users)
       .where(channel_users: { user_id: current_user.id })
+  end
+
+  def show
+    @new_timeline = Timeline.new
+    @timelines = Timeline.where(channel_id: @channel.id)
+  end
+
+  private
+
+  def set_channel
+    @channel = Channel.find(params[:id])
   end
 end
