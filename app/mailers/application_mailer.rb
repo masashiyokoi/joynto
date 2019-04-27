89d323d274c521d7c77ab6f87a588950dab61cdc
masashiyokoi <masashi.yokoi@yokoiinfocom.co.jp>
2019-04-27 12:12:53 +0900

    Change mailer sender address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'info@placck.koagen.org'
+  default from: 'norepley@ploak.koagen.org'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "placck.koagen.org", protocol: 'https' }
+  config.action_mailer.default_url_options = { host: "ploak.koagen.org", protocol: 'https' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
