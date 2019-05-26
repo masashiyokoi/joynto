7df14db5655e1f5ee41c7cddcc12ea787586a44a
masashiyokoi <masashi.yokoi@yokoiinfocom.co.jp>
2019-05-27 08:19:38 +0900

    Add corporate view

--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,6 @@ Rails.application.routes.draw do
     end
   end
   resources :comments
+  resource :corporate
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
