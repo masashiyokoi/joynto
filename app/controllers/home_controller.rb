0bdcd5cc407429e992698e57415d892e90ab9499
Masashi Yokoi <masashi.yokoi@yokoiinfocom.co.jp>
2019-10-01 22:06:25 +0900

    add new message to home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @project_activities = PublicActivity::Activity.all.where(key: 'project.create')
     @event_activities = PublicActivity::Activity.all.where(key: 'event.create')
+    @message_activities = PublicActivity::Activity.all.where(key: 'message.create')
   end
 end
