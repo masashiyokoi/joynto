0e9fa5861e552a77391f4bcd2123eb43709f807d
masashiyokoi <masashi.yokoi@yokoiinfocom.co.jp>
2019-10-11 09:14:04 +0900

    change home order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @project_activities = PublicActivity::Activity.all.where(key: 'project.create')
-    @event_activities = PublicActivity::Activity.all.where(key: 'event.create')
-    @message_activities = PublicActivity::Activity.all.where(key: 'message.create')
+    @project_activities = PublicActivity::Activity.all.where(key: 'project.create').order(id: :desc)
+    @event_activities = PublicActivity::Activity.all.where(key: 'event.create').order(id: :desc)
+    @message_activities = PublicActivity::Activity.all.where(key: 'message.create').order(id: :desc)
   end
 end
