323e6c26bd4bf1e56170437a394ddd02d63aa8fd
masashiyokoi <masashi.yokoi@yokoiinfocom.co.jp>
2020-03-01 19:52:18 +0900

    fix notifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
-  before_action :set_notifications
 
   protect_from_forgery
 
@@ -14,13 +13,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     devise_parameter_sanitizer.permit(:accept_invitation, keys: [:name])
-  end
-
-  private
-
-  def set_notifications
-    if user_signed_in?
-      @notifications = current_user.notifications
-    end
   end
 end
