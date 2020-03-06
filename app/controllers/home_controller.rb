531c9bdca2240d805298c32abdc814a1bf809b46
masashiyokoi <masashi.yokoi@yokoiinfocom.co.jp>
2020-03-06 10:40:59 +0900

    change home activities condition

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     @project_activities = PublicActivity::Activity.all.where(key: 'project.create').order(id: :desc)
     @event_activities = PublicActivity::Activity.all.where(key: 'event.create').order(id: :desc)
-    @message_activities = PublicActivity::Activity.all.where(key: 'message.create').order(id: :desc)
+    @messages = Message.joins(:channel).joins(:activities).where(activities: { key: 'message.create' }).where(channels: { kind: Channel.kinds[:times] })
   end
 end
