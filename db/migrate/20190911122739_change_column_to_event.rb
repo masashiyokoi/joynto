d76027be41eef77c86ce87401810364570af534d
EC2 Default User <ec2-user@ip-172-31-19-32.ap-northeast-1.compute.internal>
2020-06-24 08:57:25 +0900

    Remove unnecessary migrate files

--- db/migrate/20190911122739_change_column_to_event.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnToEvent < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :events, :start_datetime, :start_time
-  end
-end
--- db/migrate/20200502090143_drop_table_events.rb
+++
@@ -1,5 +0,0 @@
-class DropTableEvents < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :events
-  end
-end
